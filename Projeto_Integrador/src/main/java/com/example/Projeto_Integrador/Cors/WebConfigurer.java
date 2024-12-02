a0e1e4ac755aa4bc99994c921e6920cfc824e3d4
Pedroarchangelo123 <79946822+Pedroarchangelo123@users.noreply.github.com>
2024-12-02 14:40:01 -0300

    Update WebConfigurer.java

--- Projeto_Integrador/src/main/java/com/example/Projeto_Integrador/Cors/WebConfigurer.java
+++ Projeto_Integrador/src/main/java/com/example/Projeto_Integrador/Cors/WebConfigurer.java
@@ -11,7 +11,7 @@ public class WebConfigurer  implements WebMvcConfigurer  {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173") 
+        .allowedOrigins("http://mapbus.azurewebsites.net") 
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
         .allowedHeaders("Content-Type", "Authorization") 
         .allowCredentials(true);
