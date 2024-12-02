ec81a630189f840eaf28fe1d95948716d5f0ef89
Pedroarchangelo123 <79946822+Pedroarchangelo123@users.noreply.github.com>
2024-12-02 15:38:25 -0300

    Update WebConfigurer.java

--- Projeto_Integrador/src/main/java/com/example/Projeto_Integrador/Cors/WebConfigurer.java
+++ Projeto_Integrador/src/main/java/com/example/Projeto_Integrador/Cors/WebConfigurer.java
@@ -11,7 +11,7 @@ public class WebConfigurer  implements WebMvcConfigurer  {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("https://mapbus.netlify.app") 
+        .allowedOrigins("*") 
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
         .allowedHeaders("Content-Type", "Authorization") 
         .allowCredentials(true);
