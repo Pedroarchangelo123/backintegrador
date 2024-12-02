99d040bd9eb88f283e0b44bea7a9b44a85576c41
Pedroarchangelo123 <79946822+Pedroarchangelo123@users.noreply.github.com>
2024-12-02 14:40:34 -0300

    Update BusController.java

--- Projeto_Integrador/src/main/java/com/example/Projeto_Integrador/Controller/BusController.java
+++ Projeto_Integrador/src/main/java/com/example/Projeto_Integrador/Controller/BusController.java
@@ -24,6 +24,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("onibus")
+@CrossOrigin(origins = "https://mapbus.azurewebsites.net")
 public class BusController {
 	
 	@Autowired
